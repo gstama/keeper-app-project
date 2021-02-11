de10852c2e173a2939812180ec8c46d0b3f80963
gstama <gstama@outlook.com>
2021-02-11 18:27:55 +0000

    minor fix

--- src/components/CreateArea.jsx
+++ src/components/CreateArea.jsx
@@ -2,7 +2,6 @@ import React, { useState } from 'react';
 import AddIcon from '@material-ui/icons/Add';
 import Fab from '@material-ui/core/Fab';
 import Zoom from '@material-ui/core/Zoom';
-import HighlightIcon from '@material-ui/icons/Highlight';
 
 function CreateArea(props) {
   const [note, setNote] = useState({
